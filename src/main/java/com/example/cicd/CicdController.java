7aaf63c5663ebcb56b9fb8e2d01dfd8d820f367f
bluesun147 <bluesun147@naver.com>
2023-04-13 23:50:18 +0900

    Initial commit

---
+++ src/main/java/com/example/cicd/CicdController.java
@@ -0,0 +1,13 @@
+package com.example.cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CicdController {
+    @GetMapping("/cicd")
+    public String test() {
+        return "hello!";
+    }
+
+}
